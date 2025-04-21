8c0485ebc64285956feb4b11171b78be84a3364c
Ali Keramatipour <41532728+AliKeramatipour@users.noreply.github.com>
2025-04-21 11:44:30 +0100

    missed last value in the string

--- LemmaRightVerifier.py
+++ LemmaRightVerifier.py
@@ -4,7 +4,7 @@ from Modules import *
 err = False
 for n in range(1,15):
     fails = False
-    for i in range(n-1, 2**n - 2):
+    for i in range(n-1, 2**n - 1):
         if is_self_number(2**n + 1 + i) != is_self_number(i):
             print(f"ERROR: for {n} in {i}")
             fails = True
